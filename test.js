13127684536cc2de2e8e6f2bc460e6588333e562
pranitpingale <42433773+pranitpingale@users.noreply.github.com>
2019-11-23 17:39:27 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,24 @@
+const should = require("should");
+const request = require("request");
+const expect = require("chai").expect;
+const baseUrl = "https://swapi.co/api";
+
+const util = require("util");
+
+describe('returns luke', function() {
+    it('returns luke', function() {
+        request.get({ url: baseUrl + '/people/1/' },
+            function(error, response, body) {
+                    const bodyObj = JSON.parse(body);
+                    
+                    console.log(body);
+                    expect(bodyObj.name).to.equal("Luke Skywalker");
+                    expect(bodyObj.hair_color).to.equal("blond");
+                    expect(response.statusCode).to.equal(200);
+
+                    
+                    
+                // done();
+            });
+    });
+});
